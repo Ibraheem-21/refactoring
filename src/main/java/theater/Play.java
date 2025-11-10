46c532d0994726f756f785e04ac2238caf2454a0
Ibraheem Hussain <ibraheem.mustafa.hussain@gmail.com>
2025-11-10 12:20:52 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,25 @@
 package theater;
 
+/**
+ * File represents a play with a name and type.
+ * @null This class doesn't allow null values for any of the constructor arguments or fields.
+ */
+
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
